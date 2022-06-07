06eb1685e8833dc6bbb81bb04413ffa606e34ad9
davidnene <davienesh4@gmail.com>
2022-06-08 01:04:36 +0300

    commit tree b64bbef391d7e25cbe2ca00db71b5ca0000edd60

---
+++ index.js
@@ -0,0 +1,14 @@
+//unction expression divide, divides 2000 by 100
+function divide() {
+    return (2000 / 100);
+}
+
+//pointer function square, multiplies a number by itself
+const square = x => x * x;
+
+//pointer function add, accepting two parameters and adds them together
+const add = (x, y) => x + y;
+
+console.log(divide());
+console.log(square(3));
+console.log(add(3,7));
